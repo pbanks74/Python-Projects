41119022a93ed637d64ab582fe4337f4f44c0e6e
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-06-24 19:04:01 -0700

    Create GUI
    Created a GUI that allows the user to input text.
    Generates a web page that sets the user’s input as the body text for the web page.

---
+++ gui_webpage_gen.py
@@ -0,0 +1,48 @@
+
+import tkinter
+from tkinter import *
+import webbrowser
+
+
+class ParentWindow(Frame):
+    def __init__ (self, master):
+        Frame.__init__ (self)
+
+        self.master = master
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('{}x{}'.format(700, 300))
+        self.master.title('Webpage Generator')
+        self.master.config(bg='lightgray')
+
+        self.lblText = Label(self.master,text='Enter Text: ',font=("Helvetica", 16), fg='black', bg='lightgray')
+        self.lblText.grid(row=0, column=0,padx=(30,0),pady=(30,0))
+
+        self.txtEntry = Entry(self.master,text='{}',font=("Helvetica", 16), fg='black', bg='white')
+        self.txtEntry.grid(row=0, column=1,padx=(30,0),pady=(30,0))
+
+        self.btnSubmit = Button(self.master, text='Submit', width=10, height=2, command=self.submit)
+        self.btnSubmit.grid(row=3, column=2,padx=(0,0),pady=(30,0), sticky=SW)
+
+        self.btnCancel = Button(self.master, text='Cancel', width=10, height=2, command=self.cancel)
+        self.btnCancel.grid(row=3, column=1,padx=(0,100),pady=(30,0), sticky=SW)
+
+
+
+    def submit(self) :
+        f = open("webpage_generator.html", "w")    # Creates file if doesn't exist
+        f.write(self.txtEntry.get())   # Adds data to file
+        f.close()                                                                  
+
+        webbrowser.open_new_tab("webpage_generator.html")
+
+       
+    def cancel(self) :
+        self.master.destroy()
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
